3447dd9c5853865c3689fd43871abef002cd31a6
noglows <jessica.noglows@outlook.com>
2016-01-27 15:03:29 -0800

    Music works with keyboard input

--- noise.js
+++ noise.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
 
   var keyboard = ["c", "d", "e", "f", "g", "a", "b"];
+  var keycodes = {97: "a", 98: "b", 99: "c", 100: "d", 101: "e", 102: "f", 103: "g"};
 
   keyboard.forEach(function(key) {
     $('.' + key).click(function() {
@@ -8,5 +9,11 @@ $(document).ready( function() {
       $('#' + key + 'Audio')[0].play();
     });
   });
+
+  $(document).keypress(function(event) {
+      var pressed_key = (event.keyCode ? event.keyCode : event.which);
+      key = keycodes[pressed_key];
+      $( "." + key ).trigger( "click" );
+    });
 
 });
