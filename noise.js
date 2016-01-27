4126989d938c879202049a84ea591b9d6111015d
noglows <jessica.noglows@outlook.com>
2016-01-27 15:10:44 -0800

    CSS styles work when you use the keyboard instead of clicking

--- noise.js
+++ noise.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 
   var keyboard = ["c", "d", "e", "f", "g", "a", "b"];
-  var keycodes = {97: "a", 98: "b", 99: "c", 100: "d", 101: "e", 102: "f", 103: "g"};
+  var keycodes = {65: "a", 66: "b", 67: "c", 68: "d", 69: "e", 70: "f", 71: "g"};
 
   keyboard.forEach(function(key) {
     $('.' + key).click(function() {
@@ -10,10 +10,16 @@ $(document).ready( function() {
     });
   });
 
-  $(document).keypress(function(event) {
+  $(document).keydown(function(event) {
       var pressed_key = (event.keyCode ? event.keyCode : event.which);
       key = keycodes[pressed_key];
-      $( "." + key ).trigger( "click" );
+      $("." + key).trigger( "click" );
+      $("." + key).addClass('active');
     });
 
+  $(document).keyup(function(event) {
+      var pressed_key = (event.keyCode ? event.keyCode : event.which);
+      key = keycodes[pressed_key];
+        $("." + key).removeClass('active');
+      });
 });
